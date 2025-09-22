da55fa19ccc55d4afc10aa10285b9303d1d18da7
riyas <muhammadriyask11@gmail.com>
2025-09-22 15:31:43 +0530

    feat: add constants module for omr app

---
+++ omr/constants.py
@@ -0,0 +1,7 @@
+MEAN_INTENSITY_THRESHOLD = 40
+PDF_DPI = 300
+POPPLER_PATH = 'poppler/poppler-24.08.0/Library/bin'
+CONVERTED_IMG_PATH = 'media/processed/sheet_images/'
+RESULT_IMG_PATH = 'media/processed/marked_images/results/'
+RESULT_SHEET_PATH = 'media/results/results.csv'
+OPTIONS = ['A', 'B', 'C', 'D']
